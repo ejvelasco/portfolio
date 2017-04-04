97c4fa918e845ec729351f9a71c05f77e8e8f941
velascoDev <dracontium2@gmail.com>
2017-04-04 13:47:14 -0500

    Cleaned menu and Home code

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,10 +1,11 @@
 (function(){
 	'use strict';
-	var expanded = true;
-	var menuHeight = 80;
-	// $('#new-menu').fadeIn('slow');
+	let expanded = true;
+	let menuHeight = 80;
+	//staggered menu effect
 	window.sr = ScrollReveal({ origin: 'top', opacity: 0, duration: 1000 });
 	sr.reveal('#new-menu a', 100);
+	//change menu on scroll
 	$(window).scroll(function() {
 	    if ($(this).scrollTop() > 0 && expanded) {
 	        expanded = false;
@@ -15,29 +16,5 @@
 	    	$('#new-menu').css('background-color', '').css('padding', '').css('opacity', '');
 	    	$('#new-menu a').css('color', '');
 	    }
-	    // console.log(expanded);
-	    // console.log($(this).scrollTop());
-	});
-	//SCROLL TO DIV ON CLICK
-	$('#new-menu a').on('click', function(){
-		var tab = $(this).attr('id');
-		var section = tab.replace('-t', '');
-		if(section != 'home'){
-			console.log(updateClock)
-			clearInterval(updateClock);
-			$('#content-wrapper, #canvas, #time').fadeOut('fast');
-		}
-		if(section != 'home') {
-			$('html, body').animate({
-			        scrollTop: $("#"+section).offset().top-menuHeight+10
-			}, 1000);
-		}else{
-			$('html, body').animate({
-			        scrollTop: 0
-			}, 1000, function(){
-				$('#content-wrapper, #canvas, #time').fadeIn('fast');
-				drawClock();
-			});
-		}
 	});
 })();
