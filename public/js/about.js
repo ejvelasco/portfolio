9a3f06cab9be71e62754a136ea28144c409c1e93
velascoDev <dracontium2@gmail.com>
2017-04-24 01:13:36 -0500

    Re-Designed layout

--- public/js/about.js
+++ public/js/about.js
@@ -1,5 +1,7 @@
 (function(){
 	'use strict';
+	$('#about').hide();
+	$('#about').show();
 	// $('#code').typed({
 	// 	strings: ['dream', 'design', 'innovate', 'program.'],
 	// 	typeSpeed: 30,
@@ -10,53 +12,66 @@
 	// 		}
 	// 	}
 	// });
-	$('.marvel-device').hide();
-	$('.snippet').each(function(i, block) {
-			  		hljs.highlightBlock(block);
-				});	
-	// window.sr = ScrollReveal({ origin: 'top', opacity: 0, duration: 1000 });
-	// sr.reveal('.navbar-nav a', 300);
-	// sr.reveal('.head', { distance:'50px'}, 350);
-	$('#btn-two').click(function(){
-		$('#btn-one').stop().removeClass('btn-active');
-		$('#btn-two').stop().addClass('btn-active');
-		$('#two').stop().fadeIn('slow');
-		$('#one').stop().fadeOut('slow');
-		$('.marvel-device').stop().show();
-		$('#about-more').stop().fadeIn('slow');
-		// $('#two').fadeIn(3000);
-		// $('#code2').typed({
-		// 	strings: ['var myInterests = {\n   one: "Application Architecture",\n   two: "Application Design",\n   three: "Research-Oriented Software",\n   four: "Artificial Intelligence",\n   five: "Game Design",\n   humor() {\n      if(naughty === true){\n         God.sentence.start("callback-hell");\n      }\n   } \n};'],
-		// 	typeSpeed: 30,
-		// 	backSpeed: 30,
-		// 	callback: function(){
-				// if($('#one').css('display') !== 'block'){
-				// 	$('.typed-cursor').fadeOut('slow');
-				// }
-				// $('#code2').each(function(i, block) {
-			 //  		hljs.highlightBlock(block);
-				// });	
-			// }
-		// }); 
+	
+	// // window.sr = ScrollReveal({ origin: 'top', opacity: 0, duration: 1000 });
+	// // sr.reveal('.navbar-nav a', 300);
+	// // sr.reveal('.head', { distance:'50px'}, 350);
+	// $('#btn-two').click(function(){
+	// 	$('#btn-one').stop().removeClass('btn-active');
+	// 	$('#btn-two').stop().addClass('btn-active');
+	// 	$('#two').stop().fadeIn('slow');
+	// 	$('#one').stop().fadeOut('slow');
+	// 	$('.marvel-device').stop().show();
+	// 	$('#about-more').stop().fadeIn('slow');
+	// 	// $('#two').fadeIn(3000);
+	// 	// $('#code2').typed({
+	// 	// 	strings: ['var myInterests = {\n   one: "Application Architecture",\n   two: "Application Design",\n   three: "Research-Oriented Software",\n   four: "Artificial Intelligence",\n   five: "Game Design",\n   humor() {\n      if(naughty === true){\n         God.sentence.start("callback-hell");\n      }\n   } \n};'],
+	// 	// 	typeSpeed: 30,
+	// 	// 	backSpeed: 30,
+	// 	// 	callback: function(){
+	// 			// if($('#one').css('display') !== 'block'){
+	// 			// 	$('.typed-cursor').fadeOut('slow');
+	// 			// }
+					
+	// 		// }
+	// 	// }); 
+	// });
+	// $('#btn-one').click(function(){
+	// 	$('#btn-one').stop().addClass('btn-active');
+	// 	$('#btn-two').stop().removeClass('btn-active');
+	// 	$('#two').stop().fadeOut('slow');
+	// 	$('#about-more').stop().fadeOut('slow');
+	// 	$('#one').stop().fadeIn('slow');
+	// 	setTimeout(function(){
+	// 		$('.marvel-device').stop().fadeOut('slow');
+	// 	}, 1000);
+	// 	// $('#code').typed({
+	// 	// 	strings: ['dream', 'design', 'innovate', 'program.'],
+	// 	// 	typeSpeed: 30,
+	// 	// 	backSpeed: 30,
+	// 	// 	callback: function(){
+	// 	// 		if($('#two').css('display') !== 'block'){
+	// 	// 			$('.typed-cursor').fadeOut('slow');
+	// 	// 		}
+	// 	// 	}
+	// 	// }); 
+	// });
+	// $('').each(function(i, block) {
+	// 	hljs.highlightBlock(block);
+	// });
+	let triggerAgain = true;
+	window.sr = ScrollReveal({ origin: 'left', opacity: 0, duration: 1000 });
+	$(window).scroll(function(){
+		if( $(window).scrollTop() + 700 > $('#about').position().top  && triggerAgain === true){
+			triggerAgain = false;
+			$('.head').css('color', '#02303E');
+			$('.msg-2, .msg-3, .msg-4, .phone').show();
+		    sr.reveal('.msg-2', {delay: 0, distance:'50px', duration: 1000}, 350);	
+		    sr.reveal('.msg-4', {delay: 500, distance:'50px', duration: 1000}, 350);	
+		    sr.reveal('.msg-3', {delay: 1000, distance:'50px', duration: 1000}, 350);
+		    
+		}
 	});
-	$('#btn-one').click(function(){
-		$('#btn-one').stop().addClass('btn-active');
-		$('#btn-two').stop().removeClass('btn-active');
-		$('#two').stop().fadeOut('slow');
-		$('#about-more').stop().fadeOut('slow');
-		$('#one').stop().fadeIn('slow');
-		setTimeout(function(){
-			$('.marvel-device').stop().fadeOut('slow');
-		}, 1000);
-		// $('#code').typed({
-		// 	strings: ['dream', 'design', 'innovate', 'program.'],
-		// 	typeSpeed: 30,
-		// 	backSpeed: 30,
-		// 	callback: function(){
-		// 		if($('#two').css('display') !== 'block'){
-		// 			$('.typed-cursor').fadeOut('slow');
-		// 		}
-		// 	}
-		// }); 
-	});
+	
+	
 })();
--- public/js/menu.js
+++ public/js/menu.js
@@ -7,13 +7,15 @@
 	        expanded = false;
 	        // $('#new-menu').css('pa')
 	        $('#mobile-brand').css('top', 35);
-	        $('#new-menu').css('background-color', 'rgba(0,0,0,1)').css('padding-top', '30px').css('padding-left', '20px');
+	        $('#new-menu').css('background-color', '#EFEFEF').css('padding-top', '30px').css('padding-left', '20px');
+	        $('#arrow').stop().fadeOut('slow');
 	        // $('#new-menu').css('opacity', '.6');
 	    } else if($(this).scrollTop() === 0){
 	    	expanded = true;
 	    	$('#mobile-brand').css('top', 55);
+	    	$('#arrow').stop().fadeIn('slow');
 	    	$('#new-menu').css('background-color', '').css('padding', '').css('opacity', '');
-	    	$('#content-wrapper, canvas, #time').fadeIn(1000);
+	    	// $('#content-wrapper, canvas, #time').fadeIn(1000);
 	    }
 	});
 })();
