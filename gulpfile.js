1c93ccdaba38de9d98b3dceca1da260321352073
velascoDev <dracontium2@gmail.com>
2017-05-02 02:25:30 -0500

    Improved Responsiveness of Projects

--- gulpfile.js
+++ gulpfile.js
@@ -17,10 +17,14 @@ gulp.task('js', function() {
         .pipe(livereload())
     );
 });
-gulp.task('pug', function(){
+gulp.task('pug-1', function(){
   gulp.src('./views/*.pug')
     .pipe(livereload());
-})
+});
+gulp.task('pug-2', function(){
+  gulp.src('./views/partials/*.pug')
+    .pipe(livereload());
+});
 gulp.task('minify-css', function () {
   gulp.src('./public/css/portfolio.css')
     .pipe(importCss())
@@ -31,9 +35,10 @@ gulp.task('minify-css', function () {
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('./views/*.pug', ['pug']);
+  gulp.watch('./views/*.pug', ['pug-1']);
+  gulp.watch('./views/partials/*.pug', ['pug-2']);
   gulp.watch('./public/js/*.js', ['js']);
   gulp.watch('./public/css/*.css', ['minify-css']);
 });
 
-gulp.task('default', ['js', 'minify-css', 'watch', 'pug']);
+gulp.task('default', ['js', 'minify-css', 'watch', 'pug-1', 'pug-2']);
