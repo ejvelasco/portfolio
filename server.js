86712d6dd23a6fcca1fed6b6a954f9775d549989
velascoDev <dracontium2@gmail.com>
2017-05-31 12:55:57 -0500

    Removed unescessary middleware

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const nodemailer = require("nodemailer");
 const port = process.env.PORT || 5000;
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "pug");
