d0e9ee813cfa4ac79c0e34f9e9fe311bc2601b28
velascoDev <dracontium2@gmail.com>
2017-05-31 12:50:27 -0500

    Fixed header bug and cleaned code (send email)

--- routes/post/send-email-gh.js
+++ routes/post/send-email-gh.js
@@ -1,37 +1,38 @@
 module.exports = (app, nodemailer) => {
 	app.post("/send-email", (req, res) => {
-	    var details = req.body;
-	    var re = {
-	    	name: /^[a-zA-Z ]+$/, 
-	    	email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
+	    const details = req.body;
+	    const re = {
+	    	name: /^[a-zA-Z ]+$/,
+	    	email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 	    	msg: /\S+/
 	    };
-	    var errors = false;
-	    for(var detail in details){
-	    	if(!re[detail].test(details[detail])){
+	    let errors = false;
+	    for( const detail in details ){
+	    	if( !re[detail].test(details[detail]) ){
 	    		res.json({error: detail});
-	    		errors = true;
+				errors = true;
+				break;
 	    	}
 	    }
 	    if(!errors){
-	    var transporter = nodemailer.createTransport({
-	        service: "Gmail",
-	        auth: {
-	            user: "YOUR_EMAIL",
-	            pass: "YOUR_PWD"
-	        }
-	    });
-	    transporter.sendMail({
-	        to: "DESTINATION_EMAIL", 
-	        subject: "PORTFOLIO CONTACT", 
-	        text: details.msg+"\nName: "+details.name +"\nEmail: "+details.email
-	    }, (err) =>{
-	    	if(err && errors == false){
-	    		res.json({error: "service"});
-	    	}else{
-	    		res.json({error: false});
-	    	}
-	    });
+		    const transporter = nodemailer.createTransport({
+		        service: "YOUR_EMAIL_SERVICE",
+		        auth: {
+		            user: "YOUR_EMAIL",
+		            pass: "YOUR_PASSWORD"
+		        }
+		    });
+		    transporter.sendMail({
+		        to: "DESTINATION_EMAIL",
+		        subject: "PORTFOLIO CONTACT",
+		        text: details.msg+"\nName: "+details.name +"\nEmail: "+details.email
+		    }, (err) =>{
+		    	if( err && errors == false ){
+		    		res.json({error: "service"});
+		    	} else {
+		    		res.json({error: false});
+		    	}
+		    });
 		}
   });
 };
--- routes/post/send-email.js
+++ routes/post/send-email.js
@@ -1,37 +1,38 @@
 module.exports = (app, nodemailer) => {
 	app.post("/send-email", (req, res) => {
-	    var details = req.body;
-	    var re = {
+	    const details = req.body;
+	    const re = {
 	    	name: /^[a-zA-Z ]+$/,
 	    	email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 	    	msg: /\S+/
 	    };
-	    var errors = false;
-	    for(var detail in details){
-	    	if(!re[detail].test(details[detail])){
+	    let errors = false;
+	    for( const detail in details ){
+	    	if( !re[detail].test(details[detail]) ){
 	    		res.json({error: detail});
-	    		errors = true;
+				errors = true;
+				break;
 	    	}
 	    }
 	    if(!errors){
-	    var transporter = nodemailer.createTransport({
-	        service: "Gmail",
-	        auth: {
-	            user: "velasco810@gmail.com",
-	            pass: "getafe.99"
-	        }
-	    });
-	    transporter.sendMail({
-	        to: "eduardo.velasco@mavs.uta.edu",
-	        subject: "PORTFOLIO CONTACT",
-	        text: details.msg+"\nName: "+details.name +"\nEmail: "+details.email
-	    }, (err) =>{
-	    	if(err && errors == false){
-	    		res.json({error: "service"});
-	    	}else{
-	    		res.json({error: false});
-	    	}
-	    });
+		    const transporter = nodemailer.createTransport({
+		        service: "Gmail",
+		        auth: {
+		            user: "velasco810@gmail.com",
+		            pass: "getafe.99"
+		        }
+		    });
+		    transporter.sendMail({
+		        to: "eduardo.velasco@mavs.uta.edu",
+		        subject: "PORTFOLIO CONTACT",
+		        text: details.msg+"\nName: "+details.name +"\nEmail: "+details.email
+		    }, (err) =>{
+		    	if( err && errors == false ){
+		    		res.json({error: "service"});
+		    	} else {
+		    		res.json({error: false});
+		    	}
+		    });
 		}
   });
 };
